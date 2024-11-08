7d2115e8d3ba601ffd8c4472e08795895ec07fc1
James Teuscher <james.teuscher@outlook.com>
2024-11-08 14:33:29 -0700

    Update flashcard and flashcardEdit.jsx; Card and Deck class implement

--- src/study/flashcard.jsx
+++ src/study/flashcard.jsx
@@ -2,7 +2,24 @@ import React from "react";
 import "./study.css";
 import { NavLink } from "react-router-dom";
 
+export class Card {
+    constructor(termName, termDef, semantic) {
+        this.termName = termName
+        this.termDef = termDef
+        this.semantic = semantic
+    }
+    
+}
+
+export class Deck {
+    constructor(name, cards) {
+        this.name = name
+        this.cards = cards
+    }
+}
 export function Flashcard() {
+    let card1 = new Card("James", "Just a guy trying to learn react", "Just trying -> James")
+    let deck1 = new Deck("Test Deck",[card1] )
 return <main>
 <h1 className = "text-center">"Deck Name"</h1>
 <NavLink to="../study" className="btn btn-primary">&#8592; Back to Study</NavLink>
@@ -12,11 +29,11 @@ return <main>
         <div className="card text-center my-4 flashcard">
             <div className="flashcard-inner">
                 <div className="flashcard-front">
-                    <p>Term name</p>
-                    <p>Semantic device</p>
+                    <p>{deck1.cards[0].termName}</p>
+                    <p>{deck1.cards[0].semantic}</p>
                 </div>
                 <div className="flashcard-back">
-                    <p>Term definition</p>
+                    <p>{deck1.cards[0].termDef}</p>
                 </div>
             </div>
         </div>
@@ -29,9 +46,9 @@ return <main>
         <div className="buttons d-flex column justify-content-center my-4 ">
             <button className="btn btn-warning">Favorite</button>
             <button className="btn btn-secondary">Share Deck </button>
-            <form action="flashcardEdit.html">
-                <button type="submit" className="btn btn-primary">Edit Card</button>
-            </form>
+            
+            <NavLink to="../study/flashcardEdit" className="btn btn-primary">Edit Card</NavLink>
+            
         </div>
     </div>
 </div>
--- src/study/flashcardEdit.jsx
+++ src/study/flashcardEdit.jsx
@@ -1,6 +1,26 @@
 import React from "react";
 import "./study.css";
 import { NavLink } from "react-router-dom";
+import { Card, Deck } from "./flashcard.jsx";
+
+function getDeck(deckName){
+    // let currentDeck = localStorage.getItem(deckName)
+    // let deck = JSON.parse(currentDeck)
+    // return deck
+}
+
+let card1 = new Card("James", "Just a guy trying to learn react", "Just trying -> James")
+let deckEditing = new Deck("Test Deck",[card1] )
+
+function updateCard(){
+    // let term_name = document.getElementById('term_name').value
+    // let semantic = document.getElementById('semantic').value
+    // let definition = document.getElementById('definition').value
+    // let card = new Card(term_name, definition, semantic)
+    // deckEditing.cards[0] = card
+    // localStorage.setItem(deckEditing.name, JSON.stringify(deckEditing))
+    localStorage.setItem(formData, new FormData(document.querySelector('form')))
+}
 
 export function FlashcardEdit() {
   return <main>
@@ -8,30 +28,34 @@ export function FlashcardEdit() {
     <NavLink to="../study" className="btn btn-primary">&#8592; Back to Study</NavLink>
 
     <div className="container-fluid d-flex row justify-content-center"> 
-        <div className=" flashcard card text-center my-4 flashcard">
-            <div className="flashcard-inner">
-                <div className="flashcard-front my-4">
-                    Front of flashcard field 
-                    <input type="text" name="term_name" id="term_name" required= "true" placeholder="Term Name"/>
-                    
-                    <input type="text" required = "true" id="semantic" placeholder="Semantic Device" className="my-4"/>
+        <form onSubmit={updateCard} className="container-fluid d-flex row justify-content-center">
+            <div className=" flashcard card text-center my-4 flashcard">
+                <div className="flashcard-inner">
+                    <div className="flashcard-front my-4">
+                        Front of flashcard field
+                        <input type="text"  name="term_name" id="term_name" required= "true" placeholder={deckEditing.cards[0].termName}/>
+            
+                        <input type="text" required = "true" id="semantic" placeholder={deckEditing.cards[0].semantic} className="my-4"/>
+                    </div>
+                    <div className="flashcard-back my-4">
+                        Back of flashcard field
+                        <input type="textarea" required = "true" id= "definition" placeholder={deckEditing.cards[0].termDef}/>
+                    </div>
                 </div>
-                <div className="flashcard-back my-4">
-                    Back of flashcard field
-                    <input type="text" required = "true" id= "definition" placeholder="Definition"/> 
+            </div>
+            <div>
+                <div className="buttons d-flex column justify-content-center">
+                    <button className="btn btn-warning">Need ideas? <strong>This will use a 3rd party API to generate ideas/mnemonics</strong></button>
+                    <NavLink type="submit" to="../study/flashcard" className="btn btn-primary" action="submit" >Save Changes</NavLink>
+                </div>
+                <div className="buttons d-flex column justify-content-center my-4">
+                    <button className="btn btn-success"> &#8592; Previous Card</button>
+                    <button className="btn btn-success">Next Card &#8594;</button>
                 </div>
             </div>
-        </div>
-        <div>
-            <div className="buttons d-flex column justify-content-center">
-                <button className="btn btn-warning">Need ideas? <strong>This will use a 3rd party API to generate ideas/mnemonics</strong></button>
-                <form action="flashcardTemplate.html" ><button type="submit" className="btn btn-primary">Save Changes</button></form>
-            </div>
-            <div className="buttons d-flex column justify-content-center my-4">
-                <button className="btn btn-success"> &#8592; Previous Card</button>
-                <button className="btn btn-success">Next Card &#8594;</button>
-            </div>
-        </div>
+        </form>
+        
+        
     </div>
 </main>
 }
